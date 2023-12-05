2fc3c990c759e84dc2de7217288bbe0e75e2b60e
IbrahimKhattab172000 <ibrahimkhattab172000@gmail.com>
2023-12-05 20:57:54 +0200

    updated constants

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -5,3 +5,5 @@ Color secondaryColor = const Color(0xFF000000);
 Color cellColor = const Color(0xFF0b132b);
 Color appBarColor = const Color(0xff0b132b);
 Color white = const Color(0xffffffff);
+// Color scaffoldBackgroundColor = const Color(0xFF343541);
+Color cardColor = const Color(0xFF444654);
