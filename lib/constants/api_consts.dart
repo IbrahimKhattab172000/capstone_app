5c620bed2eef25d54995481d333ab0e0185d1b96
IbrahimKhattab172000 <ibrahimkhattab172000@gmail.com>
2023-12-06 13:56:07 +0200

    changed api key

--- lib/constants/api_consts.dart
+++ lib/constants/api_consts.dart
@@ -1,2 +1,2 @@
 String baseUrl = "https://api.openai.com/v1";
-String apiKey = "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";
+String apiKey = "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";
