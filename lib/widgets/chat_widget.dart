58c9b519b8f96f3f6b8af47b045f7d345f24f612
IbrahimKhattab172000 <ibrahimkhattab172000@gmail.com>
2023-12-06 13:55:18 +0200

    updated the chat widget

--- lib/widgets/chat_widget.dart
+++ lib/widgets/chat_widget.dart
@@ -28,7 +28,7 @@ class ChatWidget extends StatelessWidget {
               children: [
                 Image.asset(
                   chatIndex == 0
-                      ? AssetsManager.userImage
+                      ? AssetsManager.avatar
                       : AssetsManager.botImage,
                   height: 30,
                   width: 30,
@@ -66,25 +66,6 @@ class ChatWidget extends StatelessWidget {
                                   fontSize: 16),
                             ),
                 ),
-                chatIndex == 0
-                    ? const SizedBox.shrink()
-                    : const Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Icon(
-                            Icons.thumb_up_alt_outlined,
-                            color: Colors.white,
-                          ),
-                          SizedBox(
-                            width: 5,
-                          ),
-                          Icon(
-                            Icons.thumb_down_alt_outlined,
-                            color: Colors.white,
-                          )
-                        ],
-                      ),
               ],
             ),
           ),
