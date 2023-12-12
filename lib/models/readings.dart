f366e2267aa788c441ed63f4d24f46b6ce2f1c37
IbrahimKhattab172000 <ibrahimkhattab172000@gmail.com>
2023-12-12 21:06:23 +0200

    updated the reading object

--- lib/models/readings.dart
+++ lib/models/readings.dart
@@ -1,10 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Readings {
-  int flowRate;
+  int distance;
   int heartRate;
   DateTime timeStamp;
   Readings({
-    required this.flowRate,
+    required this.distance,
     required this.heartRate,
     required this.timeStamp,
   });
+
+  factory Readings.fromMap(Map<String, dynamic> map) {
+    return Readings(
+      distance: map['distance'] ?? 0,
+      heartRate: map['heartRate'] ?? 0,
+      timeStamp: (map['timeStamp'] as Timestamp).toDate(),
+    );
+  }
 }
